8ae45b87b473416cd172e75f6920fdcb1fd28041
arcataroger <arcataroger@gmail.com>
2023-09-27 09:48:20 -0700

    Squashing a bunch of commits

--- contentScript.js
+++ contentScript.js
@@ -1,255 +1,246 @@
 function page_constructor() {
-  String.prototype.clearText = function () {
-    return this.replace("’", "'")
-      .replace("–", "-")
-      .replace("®", "")
-      .replace("©", "")
-      .replace("™", "");
-  };
 
-  function nodeObserver(triggerNode, callFunction) {
-    //call callFunction at least once
-    callFunction();
+    function nodeObserver(triggerNode, callFunction) {
+        //call callFunction at least once
+        callFunction();
 
-    // Select the node that will be observed for mutations
-    const targetNode = triggerNode;
+        // Select the node that will be observed for mutations
+        const targetNode = triggerNode;
 
-    // Options for the observer (which mutations to observe)
-    const config = { attributes: true, childList: true, subtree: true };
+        // Options for the observer (which mutations to observe)
+        const config = {attributes: true, childList: true, subtree: true};
 
-    // Callback function to execute when mutations are observed
-    const callback = function (mutationList, observer) {
-      //Trigger InsertBanner when new games appears on screen.
-      callFunction();
-    };
-    // Create an observer instance linked to the callback function
-    const observer = new MutationObserver(callback);
-    // Start observing the target node for configured mutations
-    observer.observe(targetNode, config);
-  }
+        // Callback function to execute when mutations are observed
+        const callback = function (mutationList, observer) {
+            //Trigger InsertBanner when new games appears on screen.
+            callFunction();
+        };
+        // Create an observer instance linked to the callback function
+        const observer = new MutationObserver(callback);
+        // Start observing the target node for configured mutations
+        observer.observe(targetNode, config);
+    }
 
-  return Object.freeze({
-    nodeObserver,
-  });
+    return Object.freeze({
+        nodeObserver,
+    });
+}
+
+/* This simple filter function checks to see if the game's Steam ID is in the list of known GFN games */
+const isSteamIdOnGeForceNow = gameNode => {
+    const steamID = gameNode.getAttribute('data-ds-appid') // Get the game's Steam ID from its data attribute
+    return steamIdsOnGeForceNow.has(steamID); // Check it against the list of known IDs, fetched from the GFN Games Page API
 }
 
 function home_page_constructor() {
-  const { nodeObserver } = page_constructor();
-  //create html element that will be injectted to page
-  let span = "<span class='geforce-button'>GeforceNow</span>";
+    const {nodeObserver} = page_constructor();
+    //create html element that will be injected to page
+    const span = "<span class='geforce-button'>GeforceNow</span>";
 
-  function insertBanner() {
-    const gameNodes = document.querySelectorAll(".tab_item_content");
-    gameNodes.forEach((gameNode) => {
-      const game = gameNode
-        .querySelector(".tab_item_name")
-        .innerText.toLowerCase()
-        .clearText();
-
-      const gameAvailableOnGeforce = gameTitles.includes(game);
-
-      if (gameAvailableOnGeforce) {
-        let bannerDoesNotExist = !gameNode.querySelector(
-          ".tab_item_details > .geforce-button"
-        );
-        if (bannerDoesNotExist) {
-          let extensionTagDiv = gameNode.querySelector(".tab_item_details");
-          extensionTagDiv.insertAdjacentHTML("afterbegin", span);
-        }
-      }
-    });
-  }
-  nodeObserver(document.querySelector(".tabarea"), insertBanner);
-
-  return Object.freeze({
-    insertBanner,
-  });
-}
-function search_page_constructor() {
-  const { nodeObserver } = page_constructor();
-  /*Insert Geforce Now Button for Each Game payable in Geofrce */
-  //create html element that will be injectted to page
-  let span =
-    "<span class='geforce-button vr_supported' style='top:0;'>GeforceNow</span>";
-  function insertBanner() {
-    const gameNodes = document.querySelectorAll(".search_result_row");
-    gameNodes.forEach((gameNode) => {
-      const game = gameNode
-        .querySelector(".title")
-        .textContent.toLowerCase()
-        .clearText();
-
-      if (gameTitles.includes(game)) {
-        //Dont insert geforce button if geforceButton already inserted
-        let bannerDoesNotExist = !gameNode.querySelector(
-          ".responsive_search_name_combined > div > div > .geforce-button"
-        );
-        if (bannerDoesNotExist) {
-          let extensionTagDiv = gameNode.querySelector(
-            ".responsive_search_name_combined > div > div"
-          );
-          extensionTagDiv.innerHTML += span;
-        }
-      }
-    });
-  }
-  let isChecked = false;
-  /**Put Geforce Tag to Filter Options on Right Panel */
-  function insertGeforceFilter() {
-    if (isChecked) {
-      removeNonGeforceGamesFromList();
+    function insertBanner() {
+        // Iterate through the games and add banners to the GFN ones
+        [...document.querySelectorAll(".tab_item")]
+            .filter(isSteamIdOnGeForceNow)
+            .forEach(gameNode => {
+                const bannerDoesNotExist = !gameNode.querySelector(
+                    ".tab_item_details > .geforce-button"
+                );
+                if (bannerDoesNotExist) {
+                    let extensionTagDiv = gameNode.querySelector(".tab_item_details");
+                    extensionTagDiv.insertAdjacentHTML("afterbegin", span);
+                }
+            })
     }
 
-    const div = `<div id="geforce-filter"   class="tab_filter_control_row geforce-filter" data-param="tags" data-value="" data-loc="Indie">
+    nodeObserver(document.querySelector(".tabarea"), insertBanner);
+
+    return Object.freeze({
+        insertBanner,
+    });
+}
+
+function search_page_constructor() {
+    const {nodeObserver} = page_constructor();
+    /*Insert Geforce Now Button for Each Game payable in Geofrce */
+    //create html element that will be injectted to page
+    let span =
+        "<span class='geforce-button' style='top:0;'>GeforceNow</span>";
+
+    function insertBanner() {
+        // Iterate through the games and add banners to the GFN ones
+        [...document.querySelectorAll(".search_result_row")]
+            .filter(isSteamIdOnGeForceNow)
+            .forEach(gameNode => {
+                let bannerDoesNotExist = !gameNode.querySelector(
+                    ".responsive_search_name_combined > div > div > .geforce-button"
+                );
+                if (bannerDoesNotExist) {
+                    let extensionTagDiv = gameNode.querySelector(
+                        ".responsive_search_name_combined > div > div"
+                    );
+                    extensionTagDiv.innerHTML += span;
+                }
+            })
+    }
+
+    let isChecked = false;
+
+    /**Put Geforce Tag to Filter Options on Right Panel */
+
+    function insertGeforceFilter() {
+        // TODO Add "Exclude tag" functionality
+        // TODO Add proper count of filtered results
+        if (isChecked) {
+            removeNonGeforceGamesFromList();
+        }
+
+        const div = `<div id="geforce-filter"   class="tab_filter_control_row geforce-filter" data-param="tags" data-value="" data-loc="Indie">
 		<span   class="tab_filter_control tab_filter_control_include">
 			<span>
 					<span class="tab_filter_control_checkbox"></span>
 					<span class="tab_filter_control_label" style="color:#a3ff00;">
-						Geforce Now				</span>
-					<span class="tab_filter_control_count" style="">1375</span>
+						Geforce Now</span>
 			</span>
 		</span>
 					<span class="tab_filter_control_not" onclick="disableGeforceFilter()" data-icon="https://store.akamai.steamstatic.com/public/images/search_crouton_not.svg" data-loc="Indie" data-clientside="0" data-tooltip-text="Exclude results with this tag" data-gpfocus="item"><img src="https://store.akamai.steamstatic.com/public/images/search_checkbox_not.svg" width="16px" height="16px"></span>
 </div>`;
-    const filterContainer = document.getElementById("TagFilter_Container");
-    if (!document.getElementById("geforce-filter")) {
-      filterContainer.insertAdjacentHTML("afterbegin", div);
-      //add event listener of button
-      const geforceFilterElement = document.getElementById("geforce-filter");
-      geforceFilterElement.addEventListener("click", toggleGeforceFilter, true);
-      if (isChecked) {
+        const filterContainer = document.getElementById("TagFilter_Container");
+        if (!document.getElementById("geforce-filter")) {
+            filterContainer.insertAdjacentHTML("afterbegin", div);
+            //add event listener of button
+            const geforceFilterElement = document.getElementById("geforce-filter");
+            geforceFilterElement.addEventListener("click", toggleGeforceFilter, true);
+            if (isChecked) {
+                const checkInputClassList = geforceFilterElement.querySelector(
+                    ".tab_filter_control_include"
+                ).classList;
+                checkInputClassList.add("checked");
+            }
+        }
+    }
+
+    function removeNonGeforceGamesFromList() {
+        //remove node from list
+        const gameNodes = document.querySelectorAll(".search_result_row");
+        gameNodes.forEach((gameNode) => {
+            let bannerDoesNotExist = !gameNode.querySelector(
+                ".responsive_search_name_combined > div > div > .geforce-button"
+            );
+            if (bannerDoesNotExist) {
+                gameNode.style.visibility = "hidden";
+                gameNode.style.height = "0";
+                gameNode.style.margin = "0";
+                gameNode.style.border = "0";
+            }
+        });
+    }
+
+    function toggleGeforceFilter() {
+        const geforceFilterElement = document.getElementById("geforce-filter");
+        if (!geforceFilterElement) {
+            return;
+        }
         const checkInputClassList = geforceFilterElement.querySelector(
-          ".tab_filter_control_include"
+            ".tab_filter_control_include"
         ).classList;
-        checkInputClassList.add("checked");
-      }
+        if (isChecked) {
+            isChecked = false;
+            checkInputClassList.remove("checked");
+            const gameNodes = document.querySelectorAll(".search_result_row");
+            gameNodes.forEach((gameNode) => {
+                gameNode.style.visibility = "visible";
+                gameNode.style.height = "45px";
+                gameNode.style["margin-bottom"] = "5px";
+            });
+        } else {
+            isChecked = true;
+            insertGeforceFilter();
+            checkInputClassList.add("checked");
+        }
     }
-  }
-  function removeNonGeforceGamesFromList() {
-    //remove node from list
-    const gameNodes = document.querySelectorAll(".search_result_row");
-    gameNodes.forEach((gameNode) => {
-      let bannerDoesNotExist = !gameNode.querySelector(
-        ".responsive_search_name_combined > div > div > .geforce-button"
-      );
-      if (bannerDoesNotExist) {
-        gameNode.style.visibility = "hidden";
-        gameNode.style.height = "0";
-        gameNode.style.margin = "0";
-        gameNode.style.border = "0";
-      }
+
+    nodeObserver(document.getElementById("search_results"), insertBanner);
+    nodeObserver(document.getElementById("search_results"), insertGeforceFilter);
+
+    return Object.freeze({
+        insertBanner,
+        insertGeforceFilter,
     });
-  }
-
-  function toggleGeforceFilter() {
-    const geforceFilterElement = document.getElementById("geforce-filter");
-    if (!geforceFilterElement) {
-      return;
-    }
-    const checkInputClassList = geforceFilterElement.querySelector(
-      ".tab_filter_control_include"
-    ).classList;
-    if (isChecked) {
-      isChecked = false;
-      checkInputClassList.remove("checked");
-      const gameNodes = document.querySelectorAll(".search_result_row");
-      gameNodes.forEach((gameNode) => {
-        gameNode.style.visibility = "visible";
-        gameNode.style.height = "45px";
-        gameNode.style["margin-bottom"] = "5px";
-      });
-    } else {
-      isChecked = true;
-      insertGeforceFilter();
-      checkInputClassList.add("checked");
-    }
-  }
-  nodeObserver(document.getElementById("search_results"), insertBanner);
-  nodeObserver(document.getElementById("search_results"), insertGeforceFilter);
-
-  return Object.freeze({
-    insertBanner,
-    insertGeforceFilter,
-  });
 }
 
 function game_page_constructor() {
-  const { nodeObserver } = page_constructor();
-  //create html element that will be injectted to page
-  let span =
-    "<span class='geforce-button vr_supported' style='top:0;'>GeforceNow</span>";
-  function insertBanner() {
-    const gameNode = document.getElementById("appHubAppName");
-    const game = gameNode.innerText.toLowerCase().clearText();
+    const {nodeObserver} = page_constructor();
+    //create html element that will be injectted to page
+    let span =
+        "<span class='geforce-button' style='top:0; margin: 5px; vertical-align: middle'>GeforceNow</span>";
 
-    if (gameTitles.includes(game)) {
-      //Dont insert geforce button if geforceButton already inserted
-      let bannerDoesNotExist = !gameNode.querySelector(
-        "#appHubAppName > .geforce-button"
-      );
-      if (bannerDoesNotExist) {
-        gameNode.insertAdjacentHTML("beforeend", span);
-      }
+    function insertBanner() {
+        const gameNode = document.getElementById("appHubAppName");
+
+        if (isSteamIdOnGeForceNow(gameNode)) {
+            //Dont insert geforce button if geforceButton already inserted
+            let bannerDoesNotExist = !gameNode.querySelector(
+                "#appHubAppName > .geforce-button"
+            );
+            if (bannerDoesNotExist) {
+                gameNode.insertAdjacentHTML("beforeend", span);
+            }
+        }
     }
-  }
 
-  nodeObserver(document.getElementById("appHubAppName"), insertBanner);
+    nodeObserver(document.getElementById("appHubAppName"), insertBanner);
 
-  return Object.freeze({
-    insertBanner,
-  });
+    return Object.freeze({
+        insertBanner,
+    });
 }
 
 function wishlist_page_constructor() {
-  const { nodeObserver } = page_constructor();
-  //create html element that will be injectted to page
-  let span =
-    "<span class='geforce-button vr_supported' style='top:0;'>GeforceNow</span>";
-  function insertBanner() {
-    const gameNodes = document.querySelectorAll(".wishlist_row");
-    gameNodes.forEach((gameNode) => {
-      let game = gameNode
-        .querySelector(".title")
-        .textContent.trim()
-        .toLowerCase()
-        .clearText();
+    const {nodeObserver} = page_constructor();
+    //create html element that will be injectted to page
+    let span =
+        "<span class='geforce-button vr_supported' style='top:0;'>GeforceNow</span>";
 
-      if (gameTitles.includes(game)) {
-        //Dont insert geforce button if geforceButton already inserted
-        let bannerDoesNotExist = !gameNode.querySelector(
-          ".platform_icons > .geforce-button"
-        );
+    function insertBanner() {
+        // Iterate through the games and add banners to the GFN ones
+        [...document.querySelectorAll(".wishlist_row")]
+            .filter(isSteamIdOnGeForceNow)
+            .forEach((gameNode) => {
+                let bannerDoesNotExist = !gameNode.querySelector(
+                    ".platform_icons > .geforce-button"
+                );
 
-        if (bannerDoesNotExist) {
-          let extensionTagDiv = gameNode.querySelector(".platform_icons");
-          extensionTagDiv.innerHTML += span;
-        }
-      }
+                if (bannerDoesNotExist) {
+                    let extensionTagDiv = gameNode.querySelector(".platform_icons");
+                    extensionTagDiv.innerHTML += span;
+                }
+            });
+    }
+
+    nodeObserver(document.getElementById("wishlist_ctn"), insertBanner);
+
+    return Object.freeze({
+        insertBanner,
     });
-  }
-
-  nodeObserver(document.getElementById("wishlist_ctn"), insertBanner);
-
-  return Object.freeze({
-    insertBanner,
-  });
 }
 
 function setup() {
-  if (window.location.pathname === "/search/") {
-    const SearchPage = search_page_constructor();
-    SearchPage.insertGeforceFilter();
-  } else if (
-    window.location.pathname === "/" ||
-    window.location.pathname.includes("/category")
-  ) {
-    const HomePage = home_page_constructor();
-    HomePage.insertBanner;
-  } else if (window.location.pathname.includes("/app")) {
-    const GamePage = game_page_constructor();
-    GamePage.insertBanner;
-  } else if (window.location.pathname.includes("/wishlist")) {
-    wishlist_page_constructor().insertBanner;
-  }
+    if (window.location.pathname === "/search/") {
+        const SearchPage = search_page_constructor();
+        SearchPage.insertGeforceFilter();
+    } else if (
+        window.location.pathname === "/" ||
+        window.location.pathname.startsWith("/category")
+    ) {
+        const HomePage = home_page_constructor();
+        HomePage.insertBanner;
+    } else if (window.location.pathname.startsWith("/app")) {
+        const GamePage = game_page_constructor();
+        GamePage.insertBanner;
+    } else if (window.location.pathname.startsWith("/wishlist")) {
+        const WishListPage = wishlist_page_constructor();
+        WishListPage.insertBanner;
+    }
 }
+
 setup();
---
+++ getSteamIds.js
@@ -0,0 +1,80 @@
+/* This is the official API used by the GFN Games Page at https://www.nvidia.com/en-us/geforce-now/games/
+While officially undocumented, a third-party developer, Ighor July (@JulyIghor at https://github.com/JulyIghor), documented its inner workings in a blog post: https://ighor.medium.com/i-unlocked-nvidia-geforce-now-and-stumbled-upon-pirates-dc48a3f8ff7
+ */
+const GFN_API_URL = 'https://api-prod.nvidia.com/gfngames/v1/gameList'
+
+/* Fetch the games from Nvidia by requesting several pages of GraphQL results, filtered for Steam support */
+const getSteamIds = async () => {
+    let steamIdsOfGamesOnGeForceNow = new Set()
+
+    const fetchGamesQuery = /*GraphQL*/ `
+
+        # HUGE thanks to @JulyIghor for help optimizing this query!
+
+        # Reusable fragment for the body that we want
+        fragment queryFields on AppQueryType
+        {
+            
+            # We don't need this pagination info anymore since we can just fetch multiple cursors in one query
+            #            numberReturned
+            #
+            #            pageInfo {
+            #                endCursor
+            #                hasNextPage
+            #            }
+
+            items {
+                variants
+                {
+                    storeId
+                }
+            }
+        }
+
+        {
+
+            # Fetch games 0 to 1300
+            page1: apps(country:"US" appStore:"STEAM" first:1300)
+            {
+                ...queryFields
+            }
+
+            # Fetch games 1300 to 2600, though there should only be about 1600 total games as of Sept 2023
+            page2: apps(country:"US" appStore:"STEAM" first:1300 after:"MTMwMA==") # Base64 encoded "1300"
+            {
+                ...queryFields
+            }
+
+            # Future-proofing just in case. GFN probably won't grow by thousands THAT quickly
+            page3: apps(country:"US" appStore:"STEAM" first:1300 after:"MjYwMA==") # Base64 encoded "2600"
+            {
+                ...queryFields
+            }
+
+        }
+    `
+
+    const fetchConfig = {
+        body: fetchGamesQuery,
+        method: "POST",
+    };
+
+    const fetchGamesResponse = await fetch(GFN_API_URL, fetchConfig)
+    const responseJSON = await fetchGamesResponse.json()
+
+    Object.values(responseJSON.data).forEach(page => {
+        page.items.forEach((game) => {
+            if (!game.variants[0]?.storeId) {
+                return
+            }
+            steamIdsOfGamesOnGeForceNow.add(game.variants[0].storeId)
+        })
+    })
+
+    return [...steamIdsOfGamesOnGeForceNow]
+}
+
+getSteamIds().then(ids => {
+    console.log(JSON.stringify(ids))
+    console.log(`Total games listed: ${ids.length}`)
+});
