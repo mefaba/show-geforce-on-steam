507cf6391c508b25854d1f45e7f7ed76e979a1a4
arcataroger <arcataroger@gmail.com>
2023-10-06 12:31:16 -0700

    Use JS instead of JSON to get around extension async issues

--- data.js
+++
@@ -1,3 +0,0 @@
-import ids from "./latest-geforce-now-games.json" assert { type: "json" };
-// This list of IDs is from the official GFN Games page API. It's updated via a Github Action (./github/workflows/fetch-games.yml)
-const steamIdsOnGeForceNow = new Set([ids])
